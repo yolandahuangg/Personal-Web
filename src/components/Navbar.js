97617a54f0c5b7e1ec998b6c1b4aeb57a321cc0e
Yolanda Huang <102741009+yolandahuangg@users.noreply.github.com>
2023-01-10 18:43:52 -0500

    start on navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {userState} from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to='/' className="navbar-logo">
+                    //logo here
+                </Link>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
