0c34e224e6dd76b019ab25ddcf16b2db83c7dcef
Yolanda Huang <102741009+yolandahuangg@users.noreply.github.com>
2023-01-13 14:56:12 -0500

    small fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,8 +10,20 @@ function Footer() {
         </div>
         <hr></hr>
         
-        <div className='my__fotter-container'>
+        <div className='my__footer-container'>
+          <div className='my__footer-content'>
 
+            <h1 className='my__footer-content_sub sub__padding'>
+              Let's connect!
+            </h1>
+
+            <div className='my__footer-content_text text__padding'>
+              <p>
+                I am always enthusiatic to hear about:
+              </p>
+            </div>
+
+          </div>
         </div>
     </div>
   )
