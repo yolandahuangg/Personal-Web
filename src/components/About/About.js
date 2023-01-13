5033a73fefcc1e6f65b2bafc3e48d461ac085d0a
Yolanda Huang <102741009+yolandahuangg@users.noreply.github.com>
2023-01-13 00:37:27 -0500

    finish about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './About.css'
 import design1 from '../../assets/design1.png'
+import coffee from '../../assets/coffee.png'
 
 function About() {
   return (
@@ -23,7 +24,7 @@ function About() {
                 I enjoy art both digitally and traditionally. <br />
                 I am a fluent user of C/C++, Python, HTML/CSS, Javascript and Visual Basics. <br /> <br />
                 I am a huge money-burning fan for DSLR cameras. <br />
-                I love cats, movies, gaming and pretty looking notebooks. <br />
+                I love cats, movies, gaming and pretty-looking notebooks. <br />
                 I love spring. <br />
                 I love coding. <br />
                 Thanks for finding my webpage! <br />
@@ -36,9 +37,17 @@ function About() {
             </h1>
 
             <div className='my__about-content_text2 text__padding'>
-            {/* <img src={coffee} alt='coffee'/> */}
+            <img src={coffee} alt='coffee'/>
               <p>
-                I 
+                I am currently a 1B Software Engineering student at the University of Waterloo. <br /> <br />
+                Engineering Anbassador of the 2022-2023 school year. <br />
+                Yearbook Representative of our 2027 cohort. <br />
+                1A GPA of 3.95. <br />
+                Constant seeker of either coffee or tea. <br /> <br /> <br />
+                If I'm on campus you'll be able to find me in the math/eng buildings for sure. <br />
+                If not... <br />
+                I like to move around between Waterloo, Toronto and Markham and call it travelling! <br />
+
               </p>
             </div>
             
