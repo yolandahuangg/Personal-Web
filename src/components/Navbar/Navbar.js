f0f74b98c26debefe0ad99e782de21832b04a12f
Yolanda Huang <102741009+yolandahuangg@users.noreply.github.com>
2023-01-19 23:36:57 -0500

    change link

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
     <div className='my__navbar' id='home'>
       <div className='my__navbar-links'>
         <div className='my__navbar-links_container'>
-          <p><a href='#home'>HOME</a></p>
+          <p><a href='/'>HOME</a></p>
           <p><a href='#about'>ABOUT</a></p>
           <p><a href='#projects'>PROJECTS</a></p>
           <p><a href='https://drive.google.com/file/d/1BYy3khJu_F8RsvSm3Jasv96URaWJhE68/view?usp=sharing'>PORTFOLIO</a></p>
