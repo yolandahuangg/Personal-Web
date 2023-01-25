fb778b84e3cf10365a689cba406bee88edde807c
Yolanda Huang <102741009+yolandahuangg@users.noreply.github.com>
2023-01-25 16:40:53 -0500

    temp hide port

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+//import { Link } from 'react-router-dom'
 import './Navbar.css' 
 
 function Navbar() {
@@ -16,7 +16,7 @@ function Navbar() {
         <p onClick={() => ScrollTo("about")}>ABOUT</p>
         <p onClick={() => ScrollTo("projects")}>PROJECTS</p>
         <p><a href='https://drive.google.com/file/d/1BYy3khJu_F8RsvSm3Jasv96URaWJhE68/view?usp=sharing'>PORTFOLIO</a></p>
-        <p><Link to='portfolio'>PORTFOLIO</Link></p>
+        {/* <p><Link to='portfolio'>PORTFOLIO</Link></p> */}
         <p onClick={() => ScrollTo("contact")}>CONTACT</p>
       </div>
     </div>
