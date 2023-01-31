5c2440a530d00b79edbb1e89d90ece398e211205
Yolanda Huang <102741009+yolandahuangg@users.noreply.github.com>
2023-01-31 10:49:00 -0500

    add images to gallery

--- src/components/Gallery/Images.js
+++ src/components/Gallery/Images.js
@@ -1,5 +1,17 @@
 export const Images = [
     {
+        src: "https://lh4.googleusercontent.com/52csHfLNYoUsygZ3_V9yjFa5EUHWpIHOANXCHDYI3hW8OdZmqGxaNRPl_CxuPjjrkZE=w2400",
+        alt: "poster3"
+    },
+    {
+        src: "https://lh3.googleusercontent.com/_BEWDo1Ss-oVCash0rL7vrw0nEM2wav9z4X-q_MEoqOeDp2VwpSKalOmcJhzqk20PII=w2400",
+        alt: "banner1"
+    },
+    {
+        src: "https://lh6.googleusercontent.com/fkbspJO5P8yCYM4u9EKdzZJToZ-EMd3-OmbAAgs5dxqmwKejYreZDLV_Kf7B9MloZ58=w2400",
+        alt: "logo1"
+    },
+    {
         src: "https://lh3.googleusercontent.com/WmFUbYEVokByxgxSw1RKCXSSVmYNSnVjHV9EICPINfnSWXWTuokaTJRNSVke1bBgdQM=w2400",
         alt: "poster1"
     },
@@ -7,10 +19,7 @@ export const Images = [
         src: "https://lh3.googleusercontent.com/vXBM-wobm13R_Ehm7Ytm1ONfFY9V74Gb6VLwT6YZ1GIKDMnK_dFEPSodZXlFUbKUVS4=w2400",
         alt: "poster2"
     },
-    {
-        src: "https://lh5.googleusercontent.com/vB4OzGaGNwAbFUaVxQuca7sawjV0waB0whrepKgn-Zd0EeZpbZz_VnAkGvGCJKumF5k=w2400",
-        alt: "poster3"
-    },
+    
     {
         src: "https://lh3.googleusercontent.com/WkPGLJ5n_lH3NPHuSgt6Dq1-C30BYfJK4-40owBTT1Xzl-VJjPcdfwqqUqIRuwFBF-Q=w2400",
         alt: "poster4"
@@ -28,11 +37,7 @@ export const Images = [
         alt: "poster7"
     },
     {
-        src: "https://lh4.googleusercontent.com/hUc9uJKZfJqES-AcfA5JFO6b9ZLyNMxKtkBMD-JNCufdk-u5SEul6VSPyqCkgTKoeh0=w2400",
-        alt: "poster8"
-    },
-    {
         src: "https://lh4.googleusercontent.com/bhnp3Fba1FpMh-5C7zO97gt431j_PEdupwDSS-zP673BoLYCv0twctJXWIpB0Bwezgc=w2400",
-        alt: "poster9"
+        alt: "poster8"
     },
 ];
